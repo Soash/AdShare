3f0db58ac9a10c6ba45ab61ac08a0d9c629534d7
soash <soash2002@gmail.com>
2025-06-14 18:41:21 +0600

    commit

--- adshare/settings.py
+++ adshare/settings.py
@@ -133,9 +133,13 @@ else:
     
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL')
+
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'glitch.guardian.01@gmail.com'
+
+DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
